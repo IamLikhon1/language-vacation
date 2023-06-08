6f02aa9e9a484f2948af13a7b819a3d49c955407
unknown <you@example.com>
2023-06-08 08:01:12 -0700

    added insuctors page

--- src/pages/Instructors/Instructors.jsx
+++ src/pages/Instructors/Instructors.jsx
@@ -1,10 +1,22 @@
 import { useLoaderData } from "react-router-dom";
+import SingleCard from "./SingleCard";
 
 const Instructors = () => {
     const dataLoader=useLoaderData()
     console.log(dataLoader)
     return (
         <div>
+            <img className="w-full" src="https://img.freepik.com/premium-vector/businessman-explaining-scheme_23-2147504165.jpg?w=740" alt="" />
+            <div>
+                <h2 className="text-4xl text-orange-400 my-10 text-center font-serif ">Our Best Instructors</h2>
+                <div className="grid md:grid-cols-3 gap-5 mx-10 my-20">
+                    {
+                        dataLoader.map(single=><SingleCard key={single.id}
+                            single={single}
+                        ></SingleCard>)
+                    }
+                </div>
+            </div>
             
         </div>
     );
---
+++ src/pages/Instructors/SingleCard.jsx
@@ -0,0 +1,15 @@
+
+const SingleCard = ({single}) => {
+    return (
+        <div className="card w-full bg-base-100 hover:bg-orange-300 shadow-xl">
+  <figure><img className="w-36 my-5 h-36 rounded-full" src={single.image} alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title"> Name: {single.name}</h2>
+    <p className="font-medium">Email: {single.email}</p>
+    
+  </div>
+</div>
+    );
+};
+
+export default SingleCard;
