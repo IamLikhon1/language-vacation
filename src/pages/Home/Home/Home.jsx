9b474b6fa8bef1c1dc9a4ab29955664a979192f3
unknown <you@example.com>
2023-06-08 07:16:57 -0700

    fix problem

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from "../Banner/Banner";
 const Home = () => {
     return (
         <div>
+            
             {/* banner */}
             <Banner></Banner>
             {/* banner */}
