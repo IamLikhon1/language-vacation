24a642bcc8b4b1213b891a635549e9514cb76b30
unknown <you@example.com>
2023-06-10 23:56:35 +0600

    done

--- src/pages/Home/ClassesSection/ClassesSection.jsx
+++ src/pages/Home/ClassesSection/ClassesSection.jsx
@@ -11,7 +11,7 @@ const ClassesSection = () => {
             <div className="grid md:grid-cols-3 gap-6 mx-16 mt-10 mb-20">
 
            {/* card1 */}
-           <div className="card card-compact w-full hover:bg-orange-300 bg-base-100 shadow-xl">
+           <div className="card card-compact w-full hover:bg-orange-800 bg-base-100 shadow-xl">
             <figure><img className="w-full h-72 hover:scale-110" src="https://img.freepik.com/free-vector/english-teacher-concept-illustration_114360-7477.jpg?w=740&t=st=1686241702~exp=1686242302~hmac=2c424efe46d353b9568669013c00c90205b359a2d9a16fc7bf479950a3936151" alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">Classes Section: Learn English</h2>
@@ -25,7 +25,7 @@ const ClassesSection = () => {
            {/* card1 */}
 
            {/* card2 */}
-           <div className="card card-compact w-full hover:bg-purple-300 bg-base-100 shadow-xl">
+           <div className="card card-compact w-full hover:bg-purple-800 bg-base-100 shadow-xl">
             <figure><img className="w-full h-72 hover:scale-110" src="https://img.freepik.com/free-vector/arab-muslim-woman-traditional-clothing-with-blank-banner_1308-47570.jpg?w=740&t=st=1686242427~exp=1686243027~hmac=bb8668bfdba47057ed4e5e839ca271f5b2dc9fcc940f3fac8e12ee5be9350870" alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">Classes Section: Learn Arabic</h2>
@@ -39,7 +39,7 @@ const ClassesSection = () => {
            {/* card2 */}
 
            {/* card3 */}
-           <div className="card card-compact w-full hover:bg-orange-300 bg-base-100 shadow-xl">
+           <div className="card card-compact w-full hover:bg-orange-800 bg-base-100 shadow-xl">
             <figure><img className="w-full h-72 hover:scale-110" src="https://img.freepik.com/free-vector/indian-hindi-diwas-hindi-book-tree-alphabets-words-background_1035-24858.jpg?w=740&t=st=1686242500~exp=1686243100~hmac=94eee0f1f3afefe03f32fed9c326e745087201da077dfbdd804c82e8ea8caf97" alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">Classes Section: Learn Hindi</h2>
@@ -52,7 +52,7 @@ const ClassesSection = () => {
             </div>
            {/* card3 */}
            {/* card4 */}
-           <div className="card card-compact w-full hover:bg-purple-300 bg-base-100 shadow-xl">
+           <div className="card card-compact w-full hover:bg-purple-800 bg-base-100 shadow-xl">
             <figure><img className="w-full h-72 hover:scale-110" src="https://img.freepik.com/premium-vector/colorful-profession-illustration_277904-8327.jpg?w=740" alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">Classes Section: Learn French</h2>
@@ -66,7 +66,7 @@ const ClassesSection = () => {
            {/* card4 */}
 
            {/* card5 */}
-           <div className="card card-compact w-full hover:bg-orange-300 bg-base-100 shadow-xl">
+           <div className="card card-compact w-full hover:bg-orange-800 bg-base-100 shadow-xl">
             <figure><img className="w-full h-72 hover:scale-110" src="https://img.freepik.com/premium-photo/portrait-indian-lady-teacher-saree-stands-against-green-white-blackboard_466689-7102.jpg?w=740" alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">Classes Section: Learn Bangla</h2>
@@ -80,7 +80,7 @@ const ClassesSection = () => {
            {/* card5 */}
 
            {/* card6 */}
-           <div className="card card-compact w-full  hover:bg-purple-300 bg-base-100 shadow-xl">
+           <div className="card card-compact w-full  hover:bg-purple-800 bg-base-100 shadow-xl">
             <figure><img className="w-full h-72 hover:scale-110" src="https://img.freepik.com/premium-photo/blackboard-with-text-learn-spanish-flag-spain-books-pencils-compass_656538-1861.jpg?w=740" alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">Classes Section: Learn Spanish</h2>
